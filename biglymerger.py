4ecc4aa362ac7197280ef66b963fcab5a5152220
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2019-09-26 14:58:57 -0400

    Placeholder for tutorial

---
+++ biglymerger.py
@@ -0,0 +1,11 @@
+# Federico Tondolo
+# Summer 2019 at the Columbia University Medical Center
+import csv
+import os
+import subprocess
+
+def main():
+    print('Letter #:')
+    letter=input()
+
+main()
