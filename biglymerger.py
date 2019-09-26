b3441a2ccfec94c456738c0e39973b1bca559a6b
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2019-09-26 16:16:03 -0400

    Update biglymerger.py

--- biglymerger.py
+++ biglymerger.py
@@ -7,5 +7,22 @@ import subprocess
 def main():
     print('Letter #:')
     letter=input()
+    # Loops for every file inside current dir
+    for filename in sorted(os.listdir('.')):
+        # If file is a video file
+        if (filename.endswith(".asc")):
+           subprocess.call(['cp', filename, (filename[:-4] + ".txt")])
+           filename=filename[:-4]+".txt"
+           file = open(filename).read().split()
+           pos_counter=64
+           while (pos_counter<len(file)):
+               biglylist.append(file[pos_counter])
+               pos_counter+=letter+43
+               tim_counter+=0.05
+               round(tim_counter,3)
+           subprocess.call(['rm', filename])
+        # If file is not a video
+        else:
+            continue
 
 main()
